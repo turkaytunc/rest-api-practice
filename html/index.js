7556e3e2b9c80534460477f36f71b47e563cc8cb
Turkay Tunc <turkaytunc00@gmail.com>
2020-02-29 18:01:32 +0300

    Update: client side

--- html/index.js
+++ html/index.js
@@ -1,6 +1,8 @@
 const getButton = document.getElementById("get");
 const postButton = document.getElementById("post");
-const indexContent = document.getElementById("content");
+const indexContent = document.getElementById("div-content");
+const inputTitle = document.getElementById("title");
+const inputContent = document.getElementById("content");
 
 getButton.addEventListener("click", () => {
   fetch("http://localhost:8080/feed/posts")
@@ -15,4 +17,17 @@ getButton.addEventListener("click", () => {
       );
     })
     .catch(err => console.log(err));
+});
+
+postButton.addEventListener("click", () => {
+  fetch("http://localhost:8080/feed/post", {
+    method: "POST",
+    body: JSON.stringify({
+      title: inputTitle.value,
+      content: inputContent.value
+    }),
+    headers: {
+      "Content-Type": "application/json"
+    }
+  });
 });
