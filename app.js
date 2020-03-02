6b5f4088823edb428d04651ac44971c24bc8bef0
Turkay Tunc <turkaytunc00@gmail.com>
2020-03-02 22:49:27 +0300

    Added: error handle and posts fetch

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const bodyParser = require("body-parser");
 const feedRoutes = require("./routes/feed");
 const cors = require("cors");
 const mongoose = require("mongoose");
+const path = require("path");
 
 const app = express();
 const serverIp = "127.0.0.1";
@@ -12,6 +13,7 @@ const portNumber = "8080";
 app.use(cors());
 
 app.use(bodyParser.json());
+app.use("/images", express.static(path.join(__dirname, "images")));
 
 //cors fix alternative approach
 
@@ -25,6 +27,11 @@ app.use(bodyParser.json());
 //feed routes
 app.use("/feed", feedRoutes);
 
+app.use((error, req, res, next) => {
+  res.status(error.statusCode).json({ message: error.message });
+});
+
+// Database connection
 mongoose
   .connect(
     "mongodb+srv://trk:HhEIHBSxFAbwZzmB@cluster0-ledam.mongodb.net/blog?retryWrites=true&w=majority",
--- controllers/feedController.js
+++ controllers/feedController.js
@@ -3,20 +3,18 @@ const Post = require("../models/Post");
 
 //  Sends all posts to client
 exports.getPosts = (req, res, next) => {
-  res.status(200).json({
-    posts: [
-      {
-        _id: "1",
-        title: "Post Title",
-        content: "lorem ipsum content",
-        imageUrl: "images/duck.jpg",
-        creator: {
-          name: "Turkay"
-        },
-        createdAt: new Date()
+  Post.find()
+    .then(posts => {
+      res.status(200).json({
+        posts: posts
+      });
+    })
+    .catch(err => {
+      if (!err.statusCode) {
+        err.statusCode = 500;
       }
-    ]
-  });
+      next(err);
+    });
 };
 
 // Creates post with client input
@@ -24,9 +22,9 @@ exports.createPost = (req, res, next) => {
   const errors = validationResult(req);
 
   if (!errors.isEmpty()) {
-    return res
-      .status(422)
-      .json({ message: "Validation failed!!", errors: errors.array() });
+    const error = new Error("Validation failed!!");
+    error.statusCode = 422;
+    throw error;
   }
   const title = req.body.title;
   const content = req.body.content;
@@ -43,5 +41,29 @@ exports.createPost = (req, res, next) => {
       console.log(result);
       res.status(201).json({ message: "post created", post: result });
     })
-    .catch(err => console.log(err));
+    .catch(err => {
+      if (!err.statusCode) {
+        err.statusCode = 500;
+      }
+      next(err);
+    });
+};
+
+exports.getPost = (req, res, next) => {
+  const postId = req.params.postId;
+  Post.findById(postId)
+    .then(post => {
+      if (!post) {
+        const error = new Error("Could not find post");
+        error.statusCode = 404;
+        throw error;
+      }
+      res.status(200).json({ message: "Post fetched", post: post });
+    })
+    .catch(err => {
+      if (!err.statusCode) {
+        err.statusCode = 500;
+      }
+      next(err);
+    });
 };
