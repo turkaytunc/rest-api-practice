c90eed3e3432ececcb3465d799795f894796a813
Turkay Tunc <turkaytunc00@gmail.com>
2020-03-08 16:02:59 +0300

    Added: signup

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,36 @@
 const express = require("express");
+const User = require("../models/user");
+const { body } = require("express-validator/check");
+const authController = require("../controllers/auth");
+
 const router = express.Router();
 
-router.put("/signup");
+router.put(
+  "/signup",
+  [
+    body("email")
+      .trim()
+      .isEmail()
+      .withMessage("Please enter a valid email.")
+      .custom((value, { req }) => {
+        return User.findOne({ email: value }).then(userDoc => {
+          if (userDoc) {
+            return Promise.reject("E-mail address already exists!");
+          }
+        });
+      })
+      .normalizeEmail(),
+    body("password")
+      .trim()
+      .isEmpty()
+      .isLength({ min: 5 }),
+    body("name")
+      .trim()
+      .not()
+      .isEmpty()
+      .isLength({ min: 5 })
+  ],
+  authController.signup
+);
 
 module.exports = router;
